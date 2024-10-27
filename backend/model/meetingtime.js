b92138c6bfe0d91bed9d150348fa6a1efb7591d6
sachi-jh <shansalia367@gmail.com>
2024-10-27 15:47:37 -0500

    express routes

--- backend/model/meetingtime.js
+++ backend/model/meetingtime.js
@@ -4,35 +4,26 @@ const meetingTimeSchema = new mongoose.Schema({
     event_name: {
         type: String,
         required: true,
-        unique: false,
         minlength: 3,
-      },
-      users: {
-        type: Array,
-        required: true,
-      },
-      startdate: {
-        type: String,
-        required: true,
-      },
-      enddate: {
-        type: String,
-        required: true,
-      },
-      starttime: {
-        type: String,
-        required: true,
-      },
-      endtime: {
-        type: String,
-        required: true,
-      },
-      availability: {
-        type: Object,
-      },
     },
+    users: {
+        type: [String],
+        required: true,
+    },
+    startDateTime: {
+        type: Date,
+        required: true,
+    },
+    endDateTime: {
+        type: Date,
+        required: true,
+    },
+    availability: {
+        type: Object,
+    },
+},
     { timestamps: true 
 });
 
-const User = mongoose.model("User", eventSchema);
-module.exports = User;
+const Event = mongoose.model("Event", meetingTimeSchema);
+module.exports = Event;
