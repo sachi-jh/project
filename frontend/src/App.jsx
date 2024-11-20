cb35b4245bbf29496645a275e3aa30d014ccfd0f
sachi-jh <shansalia367@gmail.com>
2024-11-19 21:30:49 -0600

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,22 +5,28 @@ import './App.css'
 import ChooseAvailability from './components/ChooseAvailability'
 import CreateEvent from './components/CreateEvent'
 import ShowAvailability from './components/ShowAvailability'
+import Navbar from './components/Navbar'
 
 function App() {
   
 
   return (
     <>
+    
       <Router>
+      <Navbar />
+        <div>
+        
+
         <Routes>
-        <Route path="/createEvent" element={<CreateEvent />} />
-          <Route path="/" element={<ChooseDateTime />} />
+          <Route path="/" element={<CreateEvent />} />
+          <Route path="/chooseDateTime" element={<ChooseDateTime />} />
           <Route path="/chooseAvailability" element={<ChooseAvailability />} />
           <Route path="/showAvailability" element={<ShowAvailability />} />
         </Routes>
+        </div>
       </Router>
-      <div>
-      </div>
+      
     </>
   )
 }
